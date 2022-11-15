de348ac4e5cbf3a7ea9750f334b6f6287a6ec768
qye <qye2022@gmail.com>
2022-11-15 12:29:55 -0700

    user defined events

---
+++ src/components/School.vue
@@ -0,0 +1,31 @@
+<<template>
+  <div class="demo">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址{{address}}</h2>
+    <button @click="sendSchoolName">把学校名给App</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      name:'atguigu',
+      address:'北京'
+    }
+  },
+  props:['getSchoolName'],
+  methods: {
+    sendSchoolName(){
+      this.getSchoolName(this.name)
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .demo {
+    background-color: skyblue;
+    padding:5px;
+  }
+</style>

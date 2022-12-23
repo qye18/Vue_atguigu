dd8a2ecc05c7c7ea46d8c6419f008be8010e9ab0
qye <qye2022@gmail.com>
2022-12-23 14:44:38 -0700

    count case - using Vue

---
+++ 21_sum_case/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <Count></Count>
+  </div>
+</template>
+
+<script>
+import Count from './components/Count.vue'
+export default {
+  name:'App',
+  components: {Count},
+  
+}
+</script>
+<style>
+  .container, .foot {
+    display: flex;
+    justify-content: space-around;
+  }
+  
+  h4 {text-align: center; padding-top: 10px;}
+ 
+  ul {
+    padding-bottom: 10px;
+  }
+  img, video {
+    width: 100%;
+  }
+  img {height: 100px;}
+
+</style>
---
+++ 21_sum_case/components/Count.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="box">
+    <h4>当前 求和为：{{sum}}</h4>
+    <select v-model="number">
+      <option :value="1">1</option>
+      <option :value="2">2</option>
+      <option :value="3">3</option>
+    </select>
+    <button @click="addNumber">+</button>
+    <button @click="subNumer">-</button>
+    <button @click="oddAdd">当前和为奇数再加</button>
+    <button @click="waitAdd">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Count',
+  data() {
+    return {
+      number:1,
+      sum:0
+    }
+  },
+  methods: {
+    addNumber() {
+      this.sum += this.number;
+    },
+    subNumer() {
+      this.sum -= this.number;
+    },
+    oddAdd() {
+      if (this.sum % 2) {
+        this.sum += this.number;
+      }
+    },
+    waitAdd() {
+      setTimeout(() => {
+        this.sum += this.number;
+      },500);
+    }
+  }
+
+}
+</script>
+  
+<style scoped>
+  .box > * {
+    margin-right: 5px;
+  }
+</style>

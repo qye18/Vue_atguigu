c56b6be9d349fa387d0dfccf506e02206ed0d31e
qye <qye2022@gmail.com>
2022-12-22 19:34:25 -0700

    transition animation

---
+++ 17_src_transition_animation/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Test></Test>
+    <Test2></Test2>
+    <Test3></Test3>
+    <Test4></Test4>
+  </div>
+</template>
+
+<script>
+import Test from './components/Test.vue'
+import Test2 from './components/Test2.vue'
+import Test3 from './components/Test3.vue'
+import Test4 from './components/Test4.vue'
+export default {
+  name:'App',
+  components:{Test,Test2,Test3,Test4}
+}
+</script>
+
+<style>
+
+</style>
---
+++ 17_src_transition_animation/components/Test2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition name="hello" appear>
+      <h1 v-show="isShow" >你好啊</h1>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Test',
+  data() {
+    return {
+      isShow:true
+    }
+  },
+}
+</script>
+
+<style scoped>
+  h1{
+    background-color: orange;
+  }
+  .hello-enter, .hello-leave-to{
+    /* transform: translateX(-100%); */
+    opacity: 0;
+  }
+  .hello-enter-active,.hello-leave-active{
+    transition: 2s linear;
+  }
+  .hello-enter-to, .hello-leave{
+    /* transform: translateX(0); */
+    opacity: 1;
+  }
+ 
+
+</style>

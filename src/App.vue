a3944ffa9bef6ab751e71d36c84e8897af418000
qye <qye2022@gmail.com>
2022-12-23 14:45:26 -0700

    count case - using Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,33 @@
 <template>
-  <div class="demo">
-    <MyHeader :addTodo="addTodo"></MyHeader>
-    <MyList 
-      :todos="todos" 
-      :checkTodo="checkTodo"
-      :deleteTodo="deleteTodo"></MyList>
-    <MyFooter 
-      :todos="todos"
-      :removeAllTodos="removeAllTodos"
-      :checkAllTodos="checkAllTodos"></MyFooter>
+  <div class="container">
+    <Count></Count>
   </div>
 </template>
+
 <script>
-import MyHeader from './components/MyHeader.vue'
-import MyList from './components/MyList.vue'
-import MyFooter from './components/MyFooter.vue'
-  export default {
-    components:{MyHeader,MyList,MyFooter},
-    data() {
-      return {
-        todos:JSON.parse(localStorage.getItem('todos')) || []
-      }
-    },
-    methods: {
-      addTodo(todoObj){
-        this.todos.unshift(todoObj)
-      },
-      //勾选/取消勾选
-      checkTodo(id){
-        this.todos.forEach((todo) => {
-          if(todo.id === id) todo.done = !todo.done 
-        })
-      },
-      deleteTodo(id) {
-        const index = this.todos.findIndex(todo => todo.id === id);
-        this.todos.splice(index,1)
-      },
-      removeAllTodos(){
-        this.todos = this.todos.filter(todo => !todo.done)
-      },
-      checkAllTodos(isChecked){
-        this.todos.forEach(todo => {
-          todo.done = isChecked;
-        })
-      }
-    },
-    watch:{
-      todos:{
-        deep:true,
-        handler(value) {
-          localStorage.setItem('todos',JSON.stringify(value))
-        }
-      }
-    }
+import Count from './components/Count.vue'
+export default {
+  name:'App',
+  components: {Count},
+  mounted() {
+    console.log(this.$store);
   }
+}
 </script>
-
 <style>
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-  .demo {
-    width: 80%;
-    min-width: 300px;
-    max-width: 500px;
-    margin: 10px auto;
-    padding: 5px;
-    border: 1px solid grey;
-  }
-
-  .demo > * {
-    margin-bottom: 10px;
-    width: 100%;
-  }
-
-  button {
-    float: right;
-    cursor: pointer;
-    background-color: red; 
-    border: none;
-    border-radius: 3px;
-    color:white;
-    padding: 3px 10px;
-    vertical-align: middle;
-  }
-  button:active {
-    background-color: rgb(168, 22, 22);
+  .container, .foot {
+    display: flex;
+    justify-content: space-around;
   }
   
+  h4 {text-align: center; padding-top: 10px;}
+ 
+  ul {
+    padding-bottom: 10px;
+  }
+  img, video {
+    width: 100%;
+  }
+  img {height: 100px;}
+
 </style>
---
+++ src/components/Count.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="box">
+    <h4>当前 求和为：{{ $store.state.sum }}</h4>
+    <h4>当前 求和放大十倍后为：{{ $store.getters.BigSum }}</h4>
+    <select v-model="number">
+      <option :value="1">1</option>
+      <option :value="2">2</option>
+      <option :value="3">3</option>
+    </select>
+    <button @click="addNumber">+</button>
+    <button @click="subNumer">-</button>
+    <button @click="oddAdd">当前和为奇数再加</button>
+    <button @click="waitAdd">等一等再加</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Count',
+  data() {
+    return {
+      number:1,
+    }
+  },
+  methods: {
+    addNumber() {
+      // this.sum += this.number;
+      // this.$store.dispatch('add', this.number);
+      this.$store.commit('ADD', this.number)
+    },
+    subNumer() {
+      // this.sum -= this.number;
+      // this.$store.dispatch('sub', this.number);
+      this.$store.commit('SUB', this.number)
+    },
+    oddAdd() {
+      // if (this.sum % 2) {
+        // this.sum += this.number;
+      // }
+      this.$store.dispatch('oddAdd', this.number);
+    },
+    waitAdd() {
+      // setTimeout(() => {
+        // this.sum += this.number;
+      // },500);
+      this.$store.dispatch('waitAdd', this.number);
+    }
+  },
+
+}
+</script>
+  
+<style scoped>
+  .box > * {
+    margin-right: 5px;
+  }
+</style>

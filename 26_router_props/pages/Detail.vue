aa92d35699b9a48f37483576dff96ed600ccdbec
qye <qye2022@gmail.com>
2022-12-24 11:57:24 -0700

    router configuration-props

---
+++ 26_router_props/pages/Detail.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+      <li>消息编号：{{ $route.query.id }}</li>
+      <li>消息标题：{{ $route.query.title }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+  name:'Detail',
+  data(){
+    return {
+      details:[
+        {
+          id:'001',
+          detail:'详细信息1'
+        }
+      ]
+    }
+  },
+  props:['id', 'title']
+}
+</script>
+
+<style>
+
+</style>
---
+++ 26_router_props/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h4>Home组件的内容</h4>
+    <router-link class="navi" active-class="highlight" to="/home/news">News</router-link>
+    <router-link class="navi" active-class="highlight" to="/home/message">Message</router-link>
+    <router-view class="content-view"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+}
+</script>
+
+<style scoped>
+  .navi {
+    margin-right: 20px;
+  }
+  .highlight {
+    background-color: aqua;
+  }
+
+a {
+  text-decoration: none;  
+}
+</style>

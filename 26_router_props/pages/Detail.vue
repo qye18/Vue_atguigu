cd62ff4348087396cfd49c9577fe84929c25cb83
qye <qye2022@gmail.com>
2022-12-24 11:59:02 -0700

    update router props

--- 26_router_props/pages/Detail.vue
+++ 26_router_props/pages/Detail.vue
@@ -1,7 +1,7 @@
 <template>
     <ul>
-      <li>消息编号：{{ $route.query.id }}</li>
-      <li>消息标题：{{ $route.query.title }}</li>
+      <li>消息编号：{{ id }}</li>
+      <li>消息标题：{{ title }}</li>
     </ul>
 </template>
 

1c81e999ca29fb23d6de4fa198cf6a065d97362f
qye <qye2022@gmail.com>
2022-12-24 15:46:12 -0700

    router cache - <keep-alive> to avoid being destroyed

---
+++ 29_router_keep_alive/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h4>Home组件的内容</h4>
+    <router-link class="navi" active-class="highlight" to="/home/news">News</router-link>
+    <router-link class="navi" active-class="highlight" to="/home/message">Message</router-link>
+    <keep-alive include="News">
+      <router-view class="content-view"></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+}
+</script>
+
+<style scoped>
+  .navi {
+    margin-right: 20px;
+  }
+  .highlight {
+    background-color: aqua;
+  }
+
+a {
+  text-decoration: none;  
+}
+</style>
---
+++ 29_router_keep_alive/pages/News.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li>新闻1 <input type="text"></li>
+      <li>新闻2 <input type="text"></li>
+      <li>新闻3 <input type="text"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'News'
+}
+</script>
+
+<style>
+
+</style>

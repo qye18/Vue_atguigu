3a7f2990f07735cf07d9552a6f4cd1fe2c439789
qye <qye2022@gmail.com>
2022-12-25 13:39:56 -0700

    router guards 路由守卫

---
+++ 30_router_guards/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-xs-offset-2 col-xs-8">
+    <div class="page-header"><h2>Vue Router Demo</h2></div>
+    <button @click="back">后退</button>
+    <button @click="forward">前进</button>
+    <button @click="go">go，根据go的值后退或前进</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Banner',
+  methods:{
+    back() {
+      this.$router.back();
+    },
+    forward() {
+      this.$router.forward();
+    },
+    go() {
+      this.$router.go(2);
+    },
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 30_router_guards/pages/News.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h4 :style="{opacity}">新闻报道</h4>
+    <ul>
+      <li>新闻1 <input type="text"></li>
+      <li>新闻2 <input type="text"></li>
+      <li>新闻3 <input type="text"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'News',
+  data() {
+    return {
+      opacity:1
+    }
+  },
+  activated() {
+    this.timer = setInterval(() => {
+      console.log('挂载');
+      this.opacity -= 0.01;
+      if (this.opacity <= 0 ) {
+        this.opacity = 1;
+      }
+    }, 20);
+  },
+  deactivated() {
+    clearInterval(this.timer);
+  },
+}
+</script>
+
+<style>
+
+</style>
